acb2b86f6b43f832241d395b5bca0b8d5344f7ee
Serhan Emre Telatar <93730925+SerhanTelatar@users.noreply.github.com>
2024-07-15 17:19:30 +0300

    model_finished

--- script.py
+++ script.py
@@ -1,12 +1,1 @@
-import zipfile
-import os
 
-# ZIP dosyasının yolu
-zip_path = 'garbage-classification-v2.zip'
-extract_dir = 'data/'
-
-# ZIP dosyasını açma
-with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-    zip_ref.extractall(extract_dir)
-
-print("Dosya başarıyla açıldı ve 'data/' klasörüne yerleştirildi.")
